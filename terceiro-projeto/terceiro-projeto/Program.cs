4fa02d009a1ecb2eeb50ebc2abcffd6db46fc432
eltonsa75 <eltonsa75@hotmail.com>
2018-10-01 18:43:06 -0300

    Terceiro Exemplo
    Terceiro Exemplo

---
+++ terceiro-projeto/terceiro-projeto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace terceiro_projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string x;
+            int y;
+            double z;
+
+            /* Split utilizado para dar espaço e quebra de linha */
+            string[] vet = Console.ReadLine().Split(' ');
+
+            x = vet[0];
+            y = int.Parse(vet[1]);
+            /* CultureInfo.InvariantCulture uitlizado para substituir , por . */
+            z = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            Console.ReadLine();
+
+        }
+    }
+}
